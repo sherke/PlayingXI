8d6c14311813b2ca9641c8e1027361088629f448
sherke <66324370+sherke@users.noreply.github.com>
2021-06-01 19:26:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,260 @@
+$(document).ready(function(){
+
+    $("#down1").click(function(){
+        document.getElementById("data1").value = document.getElementById("player1").value;
+        document.getElementById("data2").value = document.getElementById("player2").value;
+
+        document.getElementById("player1").value = document.getElementById("data2").value;
+        document.getElementById("player2").value = document.getElementById("data1").value;
+    })
+
+
+    $("#up2").click(function(){
+        document.getElementById("data2").value = document.getElementById("player2").value;
+        document.getElementById("data1").value = document.getElementById("player1").value;
+
+        document.getElementById("player2").value = document.getElementById("data1").value;
+        document.getElementById("player1").value = document.getElementById("data2").value;
+    })
+    $("#down2").click(function(){
+        document.getElementById("data2").value = document.getElementById("player2").value;
+        document.getElementById("data3").value = document.getElementById("player3").value;
+
+        document.getElementById("player2").value = document.getElementById("data3").value;
+        document.getElementById("player3").value = document.getElementById("data2").value;
+    })
+
+
+    $("#up3").click(function(){
+        document.getElementById("data3").value = document.getElementById("player3").value;
+        document.getElementById("data2").value = document.getElementById("player2").value;
+
+        document.getElementById("player3").value = document.getElementById("data2").value;
+        document.getElementById("player2").value = document.getElementById("data3").value;
+    })
+    $("#down3").click(function(){
+        document.getElementById("data3").value = document.getElementById("player3").value;
+        document.getElementById("data4").value = document.getElementById("player4").value;
+
+        document.getElementById("player3").value = document.getElementById("data4").value;
+        document.getElementById("player4").value = document.getElementById("data3").value;
+    })
+
+
+    $("#up4").click(function(){
+        document.getElementById("data4").value = document.getElementById("player4").value;
+        document.getElementById("data3").value = document.getElementById("player3").value;
+
+        document.getElementById("player4").value = document.getElementById("data3").value;
+        document.getElementById("player3").value = document.getElementById("data4").value;
+    })
+    $("#down4").click(function(){
+        document.getElementById("data4").value = document.getElementById("player4").value;
+        document.getElementById("data5").value = document.getElementById("player5").value;
+
+        document.getElementById("player4").value = document.getElementById("data5").value;
+        document.getElementById("player5").value = document.getElementById("data4").value;
+    })
+
+
+    $("#up5").click(function(){
+        document.getElementById("data5").value = document.getElementById("player5").value;
+        document.getElementById("data4").value = document.getElementById("player4").value;
+
+        document.getElementById("player5").value = document.getElementById("data4").value;
+        document.getElementById("player4").value = document.getElementById("data5").value;
+    })
+    $("#down5").click(function(){
+        document.getElementById("data5").value = document.getElementById("player5").value;
+        document.getElementById("data6").value = document.getElementById("player6").value;
+
+        document.getElementById("player5").value = document.getElementById("data6").value;
+        document.getElementById("player6").value = document.getElementById("data5").value;
+    })
+
+
+    $("#up6").click(function(){
+        document.getElementById("data6").value = document.getElementById("player6").value;
+        document.getElementById("data5").value = document.getElementById("player5").value;
+
+        document.getElementById("player6").value = document.getElementById("data5").value;
+        document.getElementById("player5").value = document.getElementById("data6").value;
+    })
+    $("#down6").click(function(){
+        document.getElementById("data6").value = document.getElementById("player6").value;
+        document.getElementById("data7").value = document.getElementById("player7").value;
+
+        document.getElementById("player6").value = document.getElementById("data7").value;
+        document.getElementById("player7").value = document.getElementById("data6").value;
+    })
+
+
+    $("#up7").click(function(){
+        document.getElementById("data7").value = document.getElementById("player7").value;
+        document.getElementById("data6").value = document.getElementById("player6").value;
+
+        document.getElementById("player7").value = document.getElementById("data6").value;
+        document.getElementById("player6").value = document.getElementById("data7").value;
+    })
+    $("#down7").click(function(){
+        document.getElementById("data7").value = document.getElementById("player7").value;
+        document.getElementById("data8").value = document.getElementById("player8").value;
+
+        document.getElementById("player7").value = document.getElementById("data8").value;
+        document.getElementById("player8").value = document.getElementById("data7").value;
+    })
+
+
+    $("#up8").click(function(){
+        document.getElementById("data8").value = document.getElementById("player8").value;
+        document.getElementById("data7").value = document.getElementById("player7").value;
+
+        document.getElementById("player8").value = document.getElementById("data7").value;
+        document.getElementById("player7").value = document.getElementById("data8").value;
+    })
+    $("#down8").click(function(){
+        document.getElementById("data8").value = document.getElementById("player8").value;
+        document.getElementById("data9").value = document.getElementById("player9").value;
+
+        document.getElementById("player8").value = document.getElementById("data9").value;
+        document.getElementById("player9").value = document.getElementById("data8").value;
+    })
+
+
+    $("#up9").click(function(){
+        document.getElementById("data9").value = document.getElementById("player9").value;
+        document.getElementById("data8").value = document.getElementById("player8").value;
+
+        document.getElementById("player9").value = document.getElementById("data8").value;
+        document.getElementById("player8").value = document.getElementById("data9").value;
+    })
+    $("#down9").click(function(){
+        document.getElementById("data9").value = document.getElementById("player9").value;
+        document.getElementById("data10").value = document.getElementById("player10").value;
+
+        document.getElementById("player9").value = document.getElementById("data10").value;
+        document.getElementById("player10").value = document.getElementById("data9").value;
+    })
+
+
+    $("#up10").click(function(){
+        document.getElementById("data10").value = document.getElementById("player10").value;
+        document.getElementById("data9").value = document.getElementById("player9").value;
+
+        document.getElementById("player10").value = document.getElementById("data9").value;
+        document.getElementById("player9").value = document.getElementById("data10").value;
+    })
+    $("#down10").click(function(){
+        document.getElementById("data10").value = document.getElementById("player10").value;
+        document.getElementById("data11").value = document.getElementById("player11").value;
+
+        document.getElementById("player10").value = document.getElementById("data11").value;
+        document.getElementById("player11").value = document.getElementById("data10").value;
+    })
+
+
+    $("#up11").click(function(){
+        document.getElementById("data11").value = document.getElementById("player11").value;
+        document.getElementById("data10").value = document.getElementById("player10").value;
+
+        document.getElementById("player11").value = document.getElementById("data10").value;
+        document.getElementById("player10").value = document.getElementById("data11").value;
+    })
+    $("#down11").click(function(){
+        document.getElementById("data11").value = document.getElementById("player11").value;
+        document.getElementById("data12").value = document.getElementById("player12").value;
+
+        document.getElementById("player11").value = document.getElementById("data12").value;
+        document.getElementById("player12").value = document.getElementById("data11").value;
+    })
+
+
+    $("#up12").click(function(){
+        document.getElementById("data12").value = document.getElementById("player12").value;
+        document.getElementById("data11").value = document.getElementById("player11").value;
+
+        document.getElementById("player12").value = document.getElementById("data11").value;
+        document.getElementById("player11").value = document.getElementById("data12").value;
+    })
+    $("#down12").click(function(){
+        document.getElementById("data12").value = document.getElementById("player12").value;
+        document.getElementById("data13").value = document.getElementById("player13").value;
+
+        document.getElementById("player12").value = document.getElementById("data13").value;
+        document.getElementById("player13").value = document.getElementById("data12").value;
+    })
+
+
+    $("#up13").click(function(){
+        document.getElementById("data13").value = document.getElementById("player13").value;
+        document.getElementById("data12").value = document.getElementById("player12").value;
+
+        document.getElementById("player13").value = document.getElementById("data12").value;
+        document.getElementById("player12").value = document.getElementById("data13").value;
+    })
+    $("#down13").click(function(){
+        document.getElementById("data13").value = document.getElementById("player13").value;
+        document.getElementById("data14").value = document.getElementById("player14").value;
+
+        document.getElementById("player13").value = document.getElementById("data14").value;
+        document.getElementById("player14").value = document.getElementById("data13").value;
+    })
+
+
+    $("#up14").click(function(){
+        document.getElementById("data14").value = document.getElementById("player14").value;
+        document.getElementById("data13").value = document.getElementById("player13").value;
+
+        document.getElementById("player14").value = document.getElementById("data13").value;
+        document.getElementById("player13").value = document.getElementById("data14").value;
+    })
+    $("#down14").click(function(){
+        document.getElementById("data14").value = document.getElementById("player14").value;
+        document.getElementById("data15").value = document.getElementById("player15").value;
+
+        document.getElementById("player14").value = document.getElementById("data15").value;
+        document.getElementById("player15").value = document.getElementById("data14").value;
+    })
+
+
+    $("#up15").click(function(){
+        document.getElementById("data15").value = document.getElementById("player15").value;
+        document.getElementById("data14").value = document.getElementById("player14").value;
+
+        document.getElementById("player15").value = document.getElementById("data14").value;
+        document.getElementById("player14").value = document.getElementById("data15").value;
+    })
+    
+
+    $(".donebtn").text("Finish");
+    $(".donebtn").click(function(){
+
+        if($(".donebtn").text() == "Finish"){
+            $(".col3").css("display","none");
+            $(".col2").css("width","calc(100% - 35px)")
+            $(".donebtn").text("Rearrange");
+
+            
+        }
+        else{
+            $(".col3").css("display","flex");
+            $(".col2").css("width","calc(100% - 70px)")
+            $(".donebtn").text("Finish");
+        }
+    })
+
+
+    $(".refresh").click(function(){
+        location.reload();
+    })
+
+    $(".print").click(function(){
+
+        $(".donebtn").text(" ")
+        $(".col1").css({"font-size":"15px","font-family":"'IBM Plex Sans', sans-serif"})
+        window.print();
+        $(".donebtn").text("Finish")
+        $(".col1").css({"font-size":"21px","font-family":"Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif"})
+
+    })
+})
